dbde5d9c0f71da42c031f8f17ad5df5ed736ea06
ASMRSAEED <asmrsaeed@gmail.com>
2022-04-13 00:47:02 +0600

    Sigh Up Form Created

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,17 +10,27 @@ const Login = () => {
                 <form>
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input type="email" name="email" id="" required/>
                     </div>
                     <div className='input-group'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input type="password" name="password" id="" required/>
                     </div>
                     <input className="form-submit" type="submit" value="Login" />
                 </form>
                 <p>
                     New to Ema-John? <Link className='form-link' to="/signup">Create an Account</Link>
                 </p>
+
+                <div className='line'>
+                    <div>
+                        ================
+                    </div>
+                    <small className='middle-text'>or</small>
+                    <div>
+                        ================
+                    </div>
+                </div>
             </div>
         </div>
     );
